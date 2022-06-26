0d8433d981422bc1f87b63f72aa8e22006123a69
dokyeong <dkproh@gmail.com>
2022-06-26 23:35:59 +0900

    ControllerFactory에서 디스패쳐 서블릿의 파라미터 없이 자체로 resources에 접근했으므로 ControllerFactory 생성자 파라미터를 없앰

--- src/main/java/org/mytoypjt/controller/DispatcherServlet.java
+++ src/main/java/org/mytoypjt/controller/DispatcherServlet.java
@@ -3,8 +3,12 @@ package org.mytoypjt.controller;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class DispatcherServlet extends HttpServlet {
@@ -13,16 +17,20 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         String loc = getInitParameter("contextConfigLocation");
-//        controllerFactory = new ControllerFactory(loc);
+        controllerFactory = new ControllerFactory();
 
-
-        //
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        IController iController = null;
+        iController = controllerFactory.getController(req.getRequestURI());
+        String page = "/WEB-INF/views/index_test.jsp";
 
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/index_test.jsp");
+        if (iController != null) {
+            page = iController.execute(req, resp);
+        }
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(page);
         requestDispatcher.forward(req, resp);
     }
 }
