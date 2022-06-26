6967659a79588ad7b828d5cb548ce861a3671572
dokyeong <dkproh@gmail.com>
2022-06-26 23:35:10 +0900

    ControllerFactory에서 초기 uri와 controller 매핑 정보를 가져올 때 src/resources에서 가져와야 하는데 src 접근이 안됨. -> 빌드하면 classes폴더에 resources폴더 내부에 있는 파일이 생성되는 것을 확인함. (왜 그런지는 잘 모르겠음) -> getClass().getResource("경로")의 루트 경로가 classes폴더라는 것을 디버깅을 통해 알아냄 -> 불러오는 것 자체는 성공했음

--- src/main/java/org/mytoypjt/controller/ControllerFactory.java
+++ src/main/java/org/mytoypjt/controller/ControllerFactory.java
@@ -1,9 +1,9 @@
 package org.mytoypjt.controller;
 
-import javax.servlet.ServletContext;
-import java.io.*;
+import java.io.FileReader;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
@@ -11,20 +11,37 @@ public class ControllerFactory {
 
     private Map<String, IController> controllerMap = new HashMap<String, IController>();
 
-    ControllerFactory (String loc) {
+    @SuppressWarnings("deprecation")
+    ControllerFactory () {
         Properties properties = new Properties();
+        String uri;
+        String controllerName;
+
+        Class clazz = getClass();
+        URL url = clazz.getResource("/uri_handler_mapping.properties");
 
         try {
-            properties.load(new FileInputStream("/uri_handler_mapping.properties"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            FileReader fileReader = new FileReader(url.getPath());
+            properties.load(fileReader);
+            Iterator<Object> keyItr = properties.keySet().iterator();
+            while (keyItr.hasNext()) {
+
+                uri = (String) keyItr.next();
+                controllerName = properties.getProperty(uri);
+                Class aClass = Class.forName(controllerName);
+                IController iController = (IController) aClass.newInstance();
+
+                controllerMap.put(uri, iController);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
-    public IController getController(ServletContext servletContext, String propertyLocation) {
-
-
-        return null;
+    public IController getController(String uri) {
+        if (controllerMap.containsKey(uri)) return controllerMap.get(uri);
+        else return null;
     }
 }
