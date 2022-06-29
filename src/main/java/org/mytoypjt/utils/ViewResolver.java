cfb384066e98d1ff803f5552dddecec519307b9f
dokyeong <dkproh@gmail.com>
2022-06-29 23:58:09 +0900

    ViewResolver.getViewName() 실행 시 로직 변경 기존 : 체크없이 prefix + pagename + suffix 변경 : 맴버변수로 viewFileMap 생성하여 뷰 이름을 반환하기 전에 해당 파일이 있는지 검사. 이유 : 페이지를 찾지 못했습니다 페이지를 적용하기 위함

--- src/main/java/org/mytoypjt/utils/ViewResolver.java
+++ src/main/java/org/mytoypjt/utils/ViewResolver.java
@@ -1,12 +1,24 @@
 package org.mytoypjt.utils;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ViewResolver {
 
     String prefix = "";
     String suffix = "";
+    String rootPath = "";
+
+    Map<String, Boolean> viewFileMap;
 
     public ViewResolver() {
 
+    }
+
+    public ViewResolver(String rootPath) {
+        this.rootPath = rootPath;
+        viewFileMap = new HashMap<String, Boolean>();
     }
 
     public void setSuffix (String suffix) {
@@ -25,7 +37,26 @@ public class ViewResolver {
         return suffix;
     }
 
+    public boolean isExistPage(String pageName){
+        if (viewFileMap.containsKey(pageName)) {
+            return true;
+        }
+
+        String page = this.prefix + pageName + this.suffix;
+        page = page.substring(1);
+        page = rootPath + page;
+
+        if (!new File(page).exists())
+            return false;
+
+        viewFileMap.put(pageName, true);
+        return true;
+    }
+
     public String getViewName(String pageName) {
-        return this.prefix + pageName + this.suffix;
+        if (isExistPage(pageName))
+            return this.prefix + pageName + this.suffix;
+        else
+            return this.prefix + "pageNotFound" + this.suffix;
     }
 }
