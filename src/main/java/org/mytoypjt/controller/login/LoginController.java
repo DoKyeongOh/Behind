038318e5e4c284139f908a6aac1ce082725c4cc5
dokyeong <dkproh@gmail.com>
2022-07-01 03:18:18 +0900

    1. 로그인 기능 자체는 테스트완료.

--- src/main/java/org/mytoypjt/controller/login/LoginController.java
+++ src/main/java/org/mytoypjt/controller/login/LoginController.java
@@ -1,19 +1,23 @@
 package org.mytoypjt.controller.login;
 
-import org.mytoypjt.controller.ControllerTemplete;
+import org.mytoypjt.controller.structure.ControllerTemplete;
+import org.mytoypjt.dao.LoginDao;
+import org.mytoypjt.entity.ModelView;
+import org.mytoypjt.entity.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginController extends ControllerTemplete {
 
     @Override
-    public String executeGetRequest(HttpServletRequest req, HttpServletResponse resp) {
+    public Object executeGetRequest() {
         return "login";
     }
 
     @Override
-    public String executePostRequest(HttpServletRequest req, HttpServletResponse resp) {
+    public Object executePostRequest() {
         String userId = req.getParameter("userId");
         String userPw = req.getParameter("userPw");
 
@@ -21,10 +25,17 @@ public class LoginController extends ControllerTemplete {
         req.setAttribute("userPw", userPw);
 
         // DB 쿼리로 계정 존재 여부 체크
+        LoginDao loginDao = new LoginDao();
+        int accountNo = loginDao.getUserId(userId, userPw);
 
-        // new User();
+        if (!User.isCorrectUserNo(accountNo)) return new ModelView("login");
+        User user = loginDao.getUser(accountNo);
 
+        ModelView modelView = new ModelView("main");
 
-        return "login";
+        HttpSession httpSession = req.getSession();
+        httpSession.setAttribute("user", user);
+
+        return modelView;
     }
 }
