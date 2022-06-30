c6c06827c85993dc42bd9a3bba7339b93b48f3ee
dokyeong <dkproh@gmail.com>
2022-07-01 03:18:42 +0900

    로그인 완료 시 메인페이지로 이동해야하는데 메인페이지 간략하게 작성함

--- src/main/java/org/mytoypjt/controller/MainController.java
+++ src/main/java/org/mytoypjt/controller/MainController.java
@@ -1,2 +1,19 @@
-package org.mytoypjt.controller;public class MainController {
+package org.mytoypjt.controller;
+
+import org.mytoypjt.controller.structure.ControllerTemplete;
+import org.mytoypjt.entity.ModelView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainController extends ControllerTemplete {
+    @Override
+    public Object executeGetRequest() {
+        return "main";
+    }
+
+    @Override
+    public Object executePostRequest() {
+        return "main";
+    }
 }
