d1819b2f9119a9019542bbc01b76b680fd00114b
dokyeong <dkproh@gmail.com>
2022-07-15 23:54:29 +0900

    entity 객체인 post 추가

---
+++ src/main/java/org/mytoypjt/models/entity/Post.java
@@ -0,0 +1,36 @@
+package org.mytoypjt.models.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Post {
+
+    int postNo;
+    String title;
+    String content;
+    Date postedDate;
+    boolean is_use_anonymous_city;
+    boolean is_use_anonymous_name;
+    int commentCount;
+    int likeCount;
+    int accountNo;
+
+    public Post() {
+    }
+
+    public Post(int postNo, String title, String content, Date postedDate, boolean is_use_anonymous_city, boolean is_use_anonymous_name, int commentCount, int likeCount, int accountNo) {
+        this.postNo = postNo;
+        this.title = title;
+        this.content = content;
+        this.postedDate = postedDate;
+        this.is_use_anonymous_city = is_use_anonymous_city;
+        this.is_use_anonymous_name = is_use_anonymous_name;
+        this.commentCount = commentCount;
+        this.likeCount = likeCount;
+        this.accountNo = accountNo;
+    }
+}
