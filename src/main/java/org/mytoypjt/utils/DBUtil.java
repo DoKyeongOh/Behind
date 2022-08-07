6c741dbd889bfe782dd8bf597b2e27713c461ab4
dokyeong <dkproh@gmail.com>
2022-08-07 22:24:44 +0900

    DBCP 적용

--- src/main/java/org/mytoypjt/utils/DBUtil.java
+++ src/main/java/org/mytoypjt/utils/DBUtil.java
@@ -4,38 +4,30 @@ import org.apache.commons.dbcp2.*;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class DBUtil {
 
-    private String url = "jdbc:mysql://localhost:3306/behind?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private String id="behind_admin";
-    private String pw="1234";
-    private static DBUtil dbUtil;
+    private static String url = "jdbc:mysql://localhost:3306/behind?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static String id="behind_admin";
+    private static String pw="1234";
+    private static BasicDataSource dataSource = null;
 
-    private DBUtil(){
+    public DBUtil(){
         createConnectionPool();
-    }
-
-    public static synchronized DBUtil getInstance(){
-        if (dbUtil == null)
-            dbUtil = new DBUtil();
-        return dbUtil;
     }
 
     public Connection getConnection() {
         try {
+//            return this.dbUtil.getConnectionByDataSource();
             return DriverManager.getConnection("jdbc:apache:commons:dbcp:cp");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            return DriverManager.getConnection(url, id, pw);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-    }
-    public synchronized Connection getConnectionInPool(){
-        if (this.dbUtil == null)
-            this.dbUtil = new DBUtil();
-
-        return this.dbUtil.getConnection();
     }
 
     public void createConnectionPool() {
@@ -67,4 +59,39 @@ public class DBUtil {
         }
     }
 
+    public Connection getConnectionByDataSource(){
+        DataSource ds = getBasicDataSource();
+
+        try {
+            return ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static DataSource getBasicDataSource(){
+        if (dataSource != null) {
+            return dataSource;
+        }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        dataSource = new BasicDataSource();
+
+        dataSource.setUsername(id);
+        dataSource.setPassword(pw);
+        dataSource.setUrl(url);
+
+        dataSource.setMinIdle(10);
+        dataSource.setMaxTotal(50);
+        dataSource.setMaxOpenPreparedStatements(100);
+
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(1000L * 60L * 1L);
+        return dataSource;
+    }
 }
