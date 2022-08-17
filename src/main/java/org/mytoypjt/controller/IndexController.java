ee9cb1272ad2f526eae80b400aaee89fa8c7facd
dokyeong <dkproh@gmail.com>
2022-08-17 18:28:36 +0900

    indexController에 스프링 적용

--- src/main/java/org/mytoypjt/controller/IndexController.java
+++ src/main/java/org/mytoypjt/controller/IndexController.java
@@ -1,16 +1,34 @@
 package org.mytoypjt.controller;
 
-import org.mytoypjt.controller.structure.annotations.RequestMapping;
 import org.mytoypjt.controller.structure.properties.PropertiesControllerTemplete;
+import org.mytoypjt.models.entity.Profile;
 import org.mytoypjt.models.etc.ViewInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Controller
 public class IndexController extends PropertiesControllerTemplete {
 
-    @RequestMapping(uri = "/", method = "POST")
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView showIndexPage(@SessionAttribute(name = "profile", required = false)Profile profile){
+        if (profile != null) {
+            System.out.println("로그인 정보가 남아있어서 메인 페이지로 이동~");
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setView(new RedirectView("/main/page"));
+            return modelAndView;
+        }
+        return new ModelAndView("index");
+    }
+
+    /*@RequestMapping(uri = "/", method = "POST")
     public Object doPost() {
         return "index";
     }
@@ -22,7 +40,7 @@ public class IndexController extends PropertiesControllerTemplete {
             return ViewInfo.getRedirectViewInfo("/main/page");
 
         return new ViewInfo("index");
-    }
+    }*/
 
 }
 
