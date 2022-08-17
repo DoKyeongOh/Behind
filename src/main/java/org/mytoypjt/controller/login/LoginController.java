caab3282b82e6210b27127dc68151de5f249b5ae
dokyeong <dkproh@gmail.com>
2022-08-17 18:30:04 +0900

    로그인 기능에 대해 서비스 객체를 @Autowired로 받아오도록 수정

--- src/main/java/org/mytoypjt/controller/login/LoginController.java
+++ src/main/java/org/mytoypjt/controller/login/LoginController.java
@@ -1,6 +1,5 @@
 package org.mytoypjt.controller.login;
 
-import org.mytoypjt.controller.structure.annotations.RequestMapping;
 import org.mytoypjt.controller.structure.properties.PropertiesControllerTemplete;
 import org.mytoypjt.models.etc.ViewInfo;
 import org.mytoypjt.models.entity.Profile;
@@ -8,80 +7,59 @@ import org.mytoypjt.service.LoginService;
 import org.mytoypjt.service.annotation.Transaction;
 import org.mytoypjt.utils.ControllerUtils;
 import org.mytoypjt.utils.TransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginController extends PropertiesControllerTemplete {
+@Controller
+public class LoginController {
 
+    @Autowired
     LoginService loginService;
+
     public LoginController() {
-        loginService = (LoginService) TransactionManager.getInstance(LoginService.class);
     }
 
-    @RequestMapping(uri = "/login/page", method = "GET")
-    public ViewInfo getLoginPage(HttpServletRequest req, HttpServletResponse resp){
-        if (ControllerUtils.isExistProfileSession(req))
-            return ViewInfo.getRedirectViewInfo("/main/page");
+    @RequestMapping(path = "/login/page", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(@SessionAttribute(name = "profile", required = false)Profile profile){
+        ModelAndView modelAndView = new ModelAndView();
+        if (profile != null)
+            modelAndView.setView(new RedirectView("/main/page"));
 
-        return new ViewInfo("loginPage");
+        return new ModelAndView("loginPage");
     }
 
-    @RequestMapping(uri = "/login", method = "POST")
-    public ViewInfo getLoginSession(HttpServletRequest req, HttpServletResponse resp){
-        String id = req.getParameter("accountId");
-        String pw = req.getParameter("accountPw");
-
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public ModelAndView getLoginSession(@RequestParam(name = "accountId")String id, @RequestParam(name = "accountPw")String pw, HttpSession session){
         Profile profile = loginService.getProfile(id, pw);
 
         if (profile == null)
-            return new ViewInfo("loginPage");
+            return new ModelAndView("loginPage");
 
-        ViewInfo viewInfo = new ViewInfo();
-        viewInfo.setRedirectTo("/main/page");
-        HttpSession httpSession = req.getSession();
-        httpSession.setAttribute("profile", profile);
-
-        return viewInfo;
+        session.setAttribute("profile", profile);
+        ModelAndView mv = new ModelAndView();
+        mv.setView(new RedirectView("/main/page"));
+        return mv;
     }
 
-    @RequestMapping(uri = "/login", method = "delete")
-    public ViewInfo deleteLoginSession(HttpServletRequest req, HttpServletResponse resp){
-        HttpSession session = req.getSession();
-        session.setAttribute("profile", null);
-        return ViewInfo.getRedirectViewInfo("/");
-    }
-
-    @Override
-    public Object doGet() {
-        if (isProfileExist())
-            return "main";
-
-        return "loginPage";
-    }
-
-    @Override
-    public Object doPost() {
-        return login(req, resp);
-    }
-
-    public ViewInfo login(HttpServletRequest req, HttpServletResponse resp){
-        String id = req.getParameter("accountId");
-        String pw = req.getParameter("accountPw");
-
-        Profile profile = loginService.getProfile(id, pw);
-
-        ViewInfo viewInfo = new ViewInfo("mainPage");
+    @RequestMapping(path = "/login", method = RequestMethod.DELETE)
+    public ModelAndView deleteLoginSession(@SessionAttribute(name = "profile", required = false)Profile profile, HttpSession session){
+        ModelAndView mv = new ModelAndView("/");
         if (profile == null) {
-            viewInfo.setViewName("loginPage");
-            return viewInfo;
+            session.setAttribute("profile", null);
+            mv.setView(new RedirectView("/"));
+            return mv;
         }
-
-        viewInfo.setRedirectRequire(true);
-        HttpSession httpSession = req.getSession();
-        httpSession.setAttribute("profile", profile);
-
-        return viewInfo;
+        return mv;
     }
+
 }
--- src/main/java/org/mytoypjt/service/LoginService.java
+++ src/main/java/org/mytoypjt/service/LoginService.java
@@ -4,25 +4,25 @@ import org.mytoypjt.dao.AccountDao;
 import org.mytoypjt.dao.ProfileDao;
 import org.mytoypjt.models.entity.Profile;
 import org.mytoypjt.service.annotation.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 
+@Service
 public class LoginService {
 
-    private Connection connection = null;
+    @Autowired
+    private AccountDao accountDao;
+    @Autowired
+    private ProfileDao profileDao;
 
-    public LoginService() {
+    public LoginService() {}
 
-    }
-
-    @Transaction
     public Profile getProfile(String accountId, String accountPw){
-        AccountDao accountDao = new AccountDao();
         int accountNo = accountDao.getAccountNo(accountId, accountPw);
 
         if (!Profile.isCorrectProfileNo(accountNo)) return null;
-
-        ProfileDao profileDao = new ProfileDao();
         Profile profile = profileDao.getProfile(accountNo);
 
         return profile;
