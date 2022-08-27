365c2249f6f931ad5db81712b5009317d5fd0a85
dokyeong <dkproh@gmail.com>
2022-08-27 22:56:44 +0900

    jdbctemplete 테스트 중

--- src/main/java/org/mytoypjt/dao/AccountDao.java
+++ src/main/java/org/mytoypjt/dao/AccountDao.java
@@ -3,7 +3,9 @@ package org.mytoypjt.dao;
 import org.mytoypjt.models.entity.Account;
 import org.mytoypjt.utils.DBUtil;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +15,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class AccountDao {
@@ -24,42 +28,51 @@ public class AccountDao {
     SimpleJdbcInsert jdbcInsert;
     RowMapper<Account> accountRowMapper;
 
+
     public AccountDao(DataSource dataSource) {
         jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
         jdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("account");
+        accountRowMapper = new RowMapper<Account>() {
+            @Override
+            public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Account account = new Account(
+                        rs.getInt("account_no"),
+                        rs.getString("id"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+                return account;
+            }
+        };
     }
 
     public int getAccountNo(String id, String pw) {
-        String sql = "select account_no from account where id=? and password=?";
-        int accountNo = NOT_CORRECTED_ACCOUNT_NO;
+        String sql = "select * from account where id=:id and password=:password";
 
-        List<Account> accountList = jdbcTemplate.query(sql, accountRowMapper);
+        MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource();
+        sqlParameterSource.addValue("id", id);
+        sqlParameterSource.addValue("password", pw);
+
+        List<Account> accountList = jdbcTemplate.query(sql, sqlParameterSource, accountRowMapper);
+        if (accountList.size() > 1)
+            return -1;
+
         return accountList.get(0).getAccountNo();
-
-        /*try (
-                Connection connection = DBUtil.getBasicDataSource().getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                ) {
-            preparedStatement.setString(1, id);
-            preparedStatement.setString(2, pw);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                if (accountNo != NOT_CORRECTED_ACCOUNT_NO) return DUPLICATION_ACCOUNT;
-                accountNo = resultSet.getInt(1);
-            }
-            return accountNo;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return NOT_CORRECTED_ACCOUNT_NO;
-        }*/
     }
 
     public List<String> getAccountListByEmail(String email){
-        String sql = "select id from account where email=?";
-        List<String> idList = new ArrayList<String>();
+        String sql = "select id from account where email=:email";
 
-        try (
+        MapSqlParameterSource msps = new MapSqlParameterSource("email", email);
+
+        return jdbcTemplate.query(sql, msps, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getString("id");
+            }
+        });
+
+        /*try (
                 Connection connection = DBUtil.getBasicDataSource().getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ) {
@@ -73,7 +86,7 @@ public class AccountDao {
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
-        }
+        }*/
     }
 
     public int findAccountNo(String id, String email){
--- src/main/test/org/mytoypjt/dao/AccountDaoTest.java
+++ src/main/test/org/mytoypjt/dao/AccountDaoTest.java
@@ -1,29 +1,61 @@
 package org.mytoypjt.dao;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mytoypjt.config.WebMvcConfiguration;
+import org.mytoypjt.utils.DBUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebMvcConfiguration.class}, loader = AnnotationConfigContextLoader.class)
 class AccountDaoTest {
-
     AccountDao accountDao;
 
-    @BeforeEach()
-    public void init(){
-        accountDao = new AccountDao();
+
+    @BeforeEach
+    void executeBefore(){
+        System.out.println("It's before");
+        accountDao = new AccountDao(DBUtil.getBasicDataSource());
     }
 
     @Test
-    void isRegisteredEmail() {
-        boolean test = true;
+    void getAccountNo(){
+        boolean successed = true;
         try {
-            test = accountDao.isRegisteredEmail("dkproh@gmail.com");
-        } catch (Exception e) {
+            // test content
+
+            System.out.println(accountDao.getAccountNo("dkproh", "1234"));
+
+        }catch (Exception e) {
             e.printStackTrace();
-            test = false;
+            successed = false;
         }
-        assertEquals(true, test);
+        assertEquals(true, successed);
+
+    }
+
+    @Test
+    void getAccountListByEmail() {
+
+        boolean successed = true;
+        try {
+            // test content
+            this.accountDao.getAccountListByEmail("dkproh@gmail.com").forEach(System.out::println);
+        }catch (Exception e) {
+            e.printStackTrace();
+            successed = false;
+        }
+        assertEquals(true, successed);
     }
 }
