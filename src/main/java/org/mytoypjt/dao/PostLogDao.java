b8b447183abf4838d974de9b4ddbd486506f044c
dokyeong <dkproh@gmail.com>
2022-09-15 00:38:32 +0900

    로그를 가져오는 메서드를 수행할 때 파라미터 수정 기존 : accountNo를 사용하여 해당 유저의 전체 로그를 가져왔음 현재 : int형인 count를 추가하여 몇 개를 가져올지 정할 수 있도록 수정

--- src/main/java/org/mytoypjt/dao/PostLogDao.java
+++ src/main/java/org/mytoypjt/dao/PostLogDao.java
@@ -2,18 +2,15 @@ package org.mytoypjt.dao;
 
 import org.mytoypjt.models.entity.Post;
 import org.mytoypjt.models.entity.PostLog;
-import org.mytoypjt.utils.DBUtil;
-import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class PostLogDao {
@@ -31,14 +28,14 @@ public class PostLogDao {
                     rs.getDate("logging_date"),
                     rs.getString("action_type"),
                     rs.getInt("account_no"),
-                    rs.getInt("post_no")
+                    rs.getInt("entity_no")
             );
             return postLog;
         };
     }
 
 
-    public void writePostActivityLog(Post post, String action) {
+    public void writeLog(Post post, String action) {
         String sql = "insert into post_log (post_log_no, logging_date, action_type, account_no, post_no) " +
                 "values (null, now(), :actionType, :accountNo, :postNo)";
 
@@ -48,5 +45,15 @@ public class PostLogDao {
                 .addValue("postNo", post.getPostNo());
 
         jdbcTemplate.update(sql, param);
+    }
+
+    public List<PostLog> getLogsByAccountNo(int accountNo, int count){
+        String sql = "select * from post_log where account_no=:accountNo order by logging_date desc limit :count";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("accountNo", accountNo)
+                .addValue("count", count);
+
+        return jdbcTemplate.query(sql, param, postLogRowMapper);
     }
 }
