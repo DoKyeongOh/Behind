2e6fdc4f35acd5d4c7f5972cf97468349ee17106
dokyeong <dkproh@gmail.com>
2022-09-29 23:02:58 +0900

    minor

--- src/main/java/org/mytoypjt/service/member/MemberService.java
+++ src/main/java/org/mytoypjt/service/member/MemberService.java
@@ -43,6 +43,8 @@ public class MemberService {
             }
         });
 
+
+
         return logList;
     }
 }
--- src/main/test/org/mytoypjt/dao/TotalDaoTest.java
+++ src/main/test/org/mytoypjt/dao/TotalDaoTest.java
@@ -8,6 +8,11 @@ import org.mytoypjt.models.entity.Post;
 import org.mytoypjt.models.entity.Reply;
 import org.mytoypjt.utils.DBUtil;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class TotalDaoTest {
@@ -70,6 +75,9 @@ public class TotalDaoTest {
 
         try {
             // test content
+
+            Integer.parseInt("1141");
+
         } catch(Exception e) {
             e.printStackTrace();
             successed = false;
