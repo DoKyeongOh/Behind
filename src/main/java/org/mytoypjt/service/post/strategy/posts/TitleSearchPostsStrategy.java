cdc7080d2eeb8b33357dfda8411fa6afdcdf2493
dokyeong <dkproh@gmail.com>
2022-10-14 17:16:26 +0900

    게시글 제목으로 검색했을 시 해당 전략 객체를 동작하도록 구현.

--- src/main/java/org/mytoypjt/service/post/strategy/posts/TitleSearchPostsStrategy.java
+++ src/main/java/org/mytoypjt/service/post/strategy/posts/TitleSearchPostsStrategy.java
@@ -4,6 +4,7 @@ import org.mytoypjt.dao.PostDao;
 import org.mytoypjt.models.dto.PostSortType;
 import org.mytoypjt.models.entity.Post;
 import org.mytoypjt.models.vo.PostOption;
+import org.mytoypjt.service.post.strategy.PostConst;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,11 +21,14 @@ public class TitleSearchPostsStrategy extends BasePostsStrategy {
 
     @Override
     public PostSortType getSortType() {
-        return PostSortType.SEARCH_TITLE_FROM_USER;
+        return PostSortType.SEARCH_BY_TITLE;
     }
 
     @Override
     public List<Post> getPosts(PostOption optionVO) {
-        return null;
+        String searchWord = optionVO.getOptionMap().get(PostConst.SEARCH_WORD);
+        int pageNo = Integer.parseInt(optionVO.getPageNo());
+
+        return postDao.getPostsByTitle(pageNo, optionVO.getPostCountLimitInPage(), searchWord);
     }
 }
