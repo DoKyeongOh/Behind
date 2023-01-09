f690b9928f0babdaf7c3b9a20d29c102617d5c3b
DoKyeong Oh <dkproh@gmail.com>
2023-01-09 22:42:05 +0900

    feat : CustomException 추가
    Exception 발생 시 처리할 수 있도록 커스텀 익셉션 클래스 추가

---
+++ src/main/java/org/mytoypjt/exception/CustomException.java
@@ -0,0 +1,16 @@
+package org.mytoypjt.exception;
+
+public class CustomException extends RuntimeException {
+
+    ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+}
