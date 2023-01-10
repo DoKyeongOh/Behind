7fd9e8ed67299d02d55de8a334b71937da7249f6
DoKyeong Oh <dkproh@gmail.com>
2023-01-10 18:05:20 +0900

    feat : ErrorCode 항목 추가
    인증 및 비밀번호 변경작업 관련 enum 값 추가

--- src/main/java/org/mytoypjt/exception/ErrorCode.java
+++ src/main/java/org/mytoypjt/exception/ErrorCode.java
@@ -5,9 +5,19 @@ public enum ErrorCode {
     // 로그인 관련
     ACCOUNT_IS_NOT_EXIST,
 
-
     // 프로필 관련
     PROFILE_IS_NOT_EXIST,
+
+    // 인증 관련
+    EMAIL_IS_NOT_EXIST,
+    CERT_VALUE_IS_NOT_CORRECT,
+    CERT_VALUE_IS_NULL,
+    CERT_INPUT_IS_NULL,
+    CERT_FAILURE,
+
+    // 비밀번호 변경 관련
+    INPUT_PW_IS_NULL,
+    INPUT_PW_CHECK_IS_NULL,
 
     // 로그인 세션 관련
     ACCOUNT_IS_ALREADY_LOGIN
