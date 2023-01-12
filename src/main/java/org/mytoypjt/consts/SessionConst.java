9817ea461f20a9534bfea856420ead5dfda64c65
DoKyeong Oh <dkproh@gmail.com>
2023-01-12 14:36:20 +0900

    feat : SessionConst.REGISTRATION_CERT 추가
    - 회원가입 과정 중 인증번호를 담은 객체를 세션에 저장하기 위한 키 값(String) 추가

--- src/main/java/org/mytoypjt/consts/SessionConst.java
+++ src/main/java/org/mytoypjt/consts/SessionConst.java
@@ -3,4 +3,5 @@ package org.mytoypjt.consts;
 public class SessionConst {
     public static final String USER_PROFILE = "profile";
     public static final String POST_OPTION_KEY = "PostOption";
+    public static final String REGISTRATION_CERT = "registrationCert";
 }
