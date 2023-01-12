943741a06fa2929e5e6a8b7d6d5e849bfbbed9a7
DoKyeong Oh <dkproh@gmail.com>
2023-01-12 14:46:57 +0900

    refactor : Profile 객체 수정
    - @Setter 제거
    - primitive 타입을 wrapper 타입으로 변경 (null 체크를 위함)
    - lombok을 사용하기 때문에 필요없는 getter, setter 제거
    - 사용하지 않는 메소드 제거

--- src/main/java/org/mytoypjt/models/entity/Profile.java
+++ src/main/java/org/mytoypjt/models/entity/Profile.java
@@ -1,20 +1,20 @@
 package org.mytoypjt.models.entity;
 
 import lombok.Getter;
-import lombok.Setter;
+import org.mytoypjt.exception.CustomException;
+import org.mytoypjt.exception.ErrorCode;
 
 import java.util.Date;
 
 @Getter
-@Setter
 public class Profile {
-    int accountNo;
+    Integer accountNo;
     String nickname;
     Date registerDate;
     String city;
-    int age;
+    Integer age;
     String gender;
-    int userLevel;
+    Integer userLevel;
 
     public Profile(){}
 
@@ -28,85 +28,18 @@ public class Profile {
         this.userLevel = 1;
     }
 
-    public Profile(int accountNo, String nicName, Date registerDate, String city, int age, String gender, int userLevel) {
+    public Profile(int accountNo, String nickname, Date registerDate, String city, int age, String gender, int userLevel) {
         this.accountNo = accountNo;
-        this.nickname = nicName;
-        this.registerDate = registerDate;
-        this.city = city;
-        this.age = age;
-        this.gender = gender;
-        this.userLevel = userLevel;
-    }
-
-    public static boolean isCorrectProfile(Profile profile) {
-        if (profile == null) return false;
-        if (profile.getGender().length() > 1) return false;
-        if (profile.getNickname().equals("")) return false;
-        if (profile.getAccountNo() < 1) return false;
-        if (profile.getUserLevel() < 1) return false;
-        return true;
-    }
-
-    public int getAccountNo() {
-        return accountNo;
-    }
-
-    public void setAccountNo(int accountNo) {
-        this.accountNo = accountNo;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
         this.nickname = nickname;
-    }
-
-    public Date getRegisterDate() {
-        return registerDate;
-    }
-
-    public void setRegisterDate(Date registerDate) {
         this.registerDate = registerDate;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
         this.age = age;
-    }
-
-    public String parseGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
-    }
-
-    public int getUserLevel() {
-        return userLevel;
-    }
-
-    public void setUserLevel(int userLevel) {
         this.userLevel = userLevel;
     }
 
-    public void injectProfile(Profile profile) {
-        setNickname(profile.getNickname());
-        setCity(profile.getCity());
-        setAge(profile.getAge());
-        setGender(profile.getGender());
     }
+
+    }
+
 }
